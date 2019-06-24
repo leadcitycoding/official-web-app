e97a63acdcf03b87a6cca344ebc1dc2ee3f3bba5
Usheninte <jrandangana@gmail.com>
2019-06-24 14:01:17 +0100

    edited head html

--- components/Head.js
+++ components/Head.js
@@ -6,7 +6,7 @@ const IndexHead = () => (
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
       <link rel="icon" type="image/png" href="../static/favicon.ico" />
-      <title>Starter Next.js Web App</title>
+      <title>Lead City Coding Web App</title>
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" />
     </Head>
   </div>

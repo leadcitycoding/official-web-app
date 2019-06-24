4974c315237324438b705a2f60f68d95c8e825e4
Usheninte <jrandangana@gmail.com>
2019-06-24 14:38:14 +0100

    added copyright

--- components/Info.js
+++ components/Info.js
@@ -1,15 +1,11 @@
 const note = {
-  margin: '6em'
+  margin: '4em'
 }
 
-export default function Info() {
+export default function Copyr() {
   return(
     <div>
-      <h4 style={note}>More info at <a href="https://nextjs.org/" target="_blank">nextjs.org</a></h4>
-
-      <h5 style={note}>Clone the <a href="https://github.com/Usheninte/starter-nextjs"><i class="fab fa-2x fa-github"></i>  repo</a></h5>
-
-      <h7>Built by <a href="https://usheninte.xyz">@Usheninte</a>. Open Source :)</h7>
+      <h4 style={note}>Lead City University &copy; All Rights Reserved</h4>
     </div>
   );
 }

60d2e37bc9fe4bff047d27cc1b659972aceabe87
Usheninte <jrandangana@gmail.com>
2019-06-26 03:29:57 +0100

    added description

--- components/Head.js
+++ components/Head.js
@@ -6,6 +6,7 @@ const IndexHead = () => (
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
       <link rel="icon" type="image/png" href="../static/favicon.ico" />
+      <meta name="description" content="Software Development, is the future of the African workplace, and economy. For this reason, Lead City Coding Academy will be offering a two week, intensive Software Engineering Bootcamp, at Lead City University - Ibadan. This innovative learning programme, will start in the second week of July, and application is open to all students and the general population." />
       <title>Lead City Coding Web App</title>
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" />
     </Head>
